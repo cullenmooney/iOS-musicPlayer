73632cc81aa301ac4c510ba32430d635b4655bc1
Cullen Mooney <cullenmooney@Cullens-MacBook.local>
2017-11-29 12:22:12 -0600

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Cullen Mooney on 11/29/17.
+//  Copyright © 2017 Cullen Mooney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
