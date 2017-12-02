5075c018340add3ddf5b6d9feb39ae0c9f97edab
Cullen Mooney <cullenmooney@Cullens-MacBook.local>
2017-12-02 16:28:14 -0600

    basic functionality is there

---
+++ MusicPlayer/GenreButtonScreen.swift
@@ -0,0 +1,35 @@
+//
+//  GenreButtonScreen.swift
+//  MusicPlayer
+//
+//  Created by Cullen Mooney on 11/29/17.
+//  Copyright © 2017 Cullen Mooney. All rights reserved.
+//
+
+import UIKit
+
+class GenreButtonScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
